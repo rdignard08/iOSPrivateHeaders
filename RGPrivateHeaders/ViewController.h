5f648d50760133f090c2df697663338e8760fe1e
Ryan Dignard <conceptuallyflawed@gmail.com>
2015-04-14 15:33:24 -0700

    Initial Commit

---
+++ RGPrivateHeaders/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RGPrivateHeaders
+//
+//  Created by Ryan Dignard on 4/14/15.
+//  Copyright (c) 2015 RyanDignard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
